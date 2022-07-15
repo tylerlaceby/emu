7b3be06fff0f156445945a92033a4ded50019af1
Tyler Laceby <tylerbadgertutorials@gmail.com>
2022-07-15 14:38:04 -0700

    Added REPL & CLI Args checks

--- src/emu.cpp
+++ src/emu.cpp
@@ -2,12 +2,43 @@
 #include "parser/parser.h"
 #include "runtime/interpreter.h"
 
-//
-int main () {
-    std::string code = emu::io::read_file_to_string ("tests/syntax.emu");
-    Parser parser;
+#include <string.h>
 
-    Program* tree = parser.parse(code);
-    print_ast(tree, 0);
+//
+int main (const int argc, const char** args) {
+    bool debugAST = false;
+    bool repl = false;
+
+    // Handle CLI Args
+    for (int i = 0; i < argc; i++) {
+        if (strcmp(args[i], "--debug-ast") == 0)
+            debugAST = true;
+        if (strcmp(args[i], "--repl") == 0)
+            repl = true;
+    }
+
+    Parser parser;
+    Program* program;
+    if (repl) {
+        std::string buffer;
+        std::string code;
+        printf("emu language : version 0.1\n");
+        while (true) {
+            printf(">> ");
+            std::getline(std::cin, buffer);
+            if (buffer.compare("close()") == 0) break;
+            else if (buffer.compare("exit()") == 0) break;
+            else if (buffer.compare("quit()") == 0) break;
+
+            code = std::string(buffer);
+            program = parser.parse(code);
+            if (debugAST) print_ast(program, 0);
+        }
+        
+    } else {
+        std::string code = emu::io::read_file_to_string ("tests/syntax.emu");
+        program = parser.parse(code);
+        if (debugAST) print_ast(program, 0);
+    }
     return 0;
 }
